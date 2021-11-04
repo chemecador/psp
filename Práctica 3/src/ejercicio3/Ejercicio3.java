43ae4951d57d30be36aab452d115c54632fe66b3
alexgg <alejandrogata7@gmail.com>
2021-11-04 13:54:40 +0100

    4-11 Slider

--- Práctica 3/src/ejercicio3/Ejercicio3.java
+++ Práctica 3/src/ejercicio3/Ejercicio3.java
@@ -1,14 +1,71 @@
 package ejercicio3;
 
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.Scanner;
+
+
+class Slider extends Observable{
+    static JSlider slider; /*se deben instanciar antes para poder usarlos en el método creado mas abajo */
+    static JLabel label; /*y a la vez estaticos (static) también para poder ser usados en nuestro método creado. */
+    static JButton aceptar;
+    public int getValue(){
+        return slider.getValue();
+    }
+
+    public Slider() {
+
+        JFrame ventana = new JFrame("Termostato");
+        ventana.setSize(400, 400);
+        JPanel panel = new JPanel();
+        aceptar = new JButton("OK");
+        aceptar.addActionListener(this::actionPerformed);
+        slider = new JSlider(JSlider.VERTICAL, 15, 25, 21);
+        slider.setInverted(false);
+        slider.setPaintTicks(true);
+        slider.setMajorTickSpacing(5);
+        slider.setMinorTickSpacing(1);
+        slider.setPaintLabels(true);
+        slider.addChangeListener(new Termostato());
+        label = new JLabel("Introduce la temperatura");
+        panel.add(label);
+        panel.add(aceptar);
+        panel.add(slider);
+        ventana.add(panel);
+        ventana.setVisible(true);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==aceptar) {
+            System.exit(0);
+        }
+    }
+
+    public class Termostato implements ChangeListener {
+        public void stateChanged(ChangeEvent e) {
+            int grados = slider.getValue();
+            String s = Integer.toString(grados);
+            label.setText(s);
+        }
+    }
+
+}
 
 class Observado extends Observable {
 
     private static Observado termostato = new Observado(); //instancia única
+    private static Slider termoSlider;
     int temperatura; //temperatura general
+    Slider slider;
+
+    private Observado() {
+        this.termoSlider = new Slider();
+        this.temperatura = 0;
+    }
 
     //getter
     public static Observado getInstance() {
@@ -17,19 +74,6 @@ class Observado extends Observable {
         }
         return termostato;
     }
-
-    //método que permite cambiar la temperatura
-    public void cambiarTemperatura() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Introduce la temperatura: ");
-        this.temperatura = in.nextInt();
-        //Marcamos el objeto observable como objeto que ha cambiado
-        setChanged();
-        //Notificamos a los observadores y le enviamos el nuevo valor
-        notifyObservers(temperatura);
-    }
-
-
 }
 
 class Observador implements Observer {
@@ -52,7 +96,7 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         Observado observado = Observado.getInstance(); //se obtiene la instancia única
         ArrayList<Observador> observadores = new ArrayList<>(); //se crea una lista de observadores vacía
-
+        Slider miSlider = new Slider();
         //se crean 4 observadores y se añaden a la lista de observadores
         for (int i = 1; i <= 4; i++) {
             Observador observador = new Observador(i);
@@ -61,8 +105,9 @@ public class Ejercicio3 {
         //para cada clase observada, se añaden los 4 observadores
         for (Observador observador : observadores) {
             observado.addObserver(observador);
+            miSlider.addObserver(observador);
         }
 
-        observado.cambiarTemperatura(); //se invoca el método para cambiar la temperatura
+        //observado.cambiarTemperatura(); //se invoca el método para cambiar la temperatura
     }
 }
