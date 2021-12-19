34102bb4c8c33433b4931b09a18648ffbc1a674d
chemecador <alejandrogata7@gmail.com>
2021-12-19 17:30:15 +0100

    P5EJ4 Falta empate

--- Practica 5/src/ejercicio4/Cliente.java
+++ Practica 5/src/ejercicio4/Cliente.java
@@ -17,23 +17,174 @@ public class Cliente extends Vista implements ActionListener, Runnable {
     private Socket conn;
     private DataInputStream in;
     private DataOutputStream out;
-    private int[][] t;
+    private static int[][] t;
     private String s;
     private boolean turno;
     private char id;
+    private int contadorTrampas;
 
 
     public Cliente() {
+        try {
+            conn = new Socket(HOST, PUERTO);
+            in = new DataInputStream(conn.getInputStream());
+            out = new DataOutputStream(conn.getOutputStream());
+        } catch (IOException e) {
+            System.out.println("Error al conectar el Cliente con el Servidor");
+            e.printStackTrace();
+        }
+        iniciar();
         new Thread(this).start();
     }
 
-    private void iniciar() {
-        t = new int[TOTAL_FILAS][TOTAL_COLUMNAS];
-        for (int i = 0; i < TOTAL_FILAS; i++) {
-            for (int j = 0; j < TOTAL_COLUMNAS; j++) {
-                this.t[i][j] = 0;
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                s = in.readUTF();
+                if (s.equals("W")){
+                    JOptionPane.showMessageDialog(null,
+                            "Bien jugado, campeón.",
+                        "¡Has ganado!",
+                        JOptionPane.PLAIN_MESSAGE);
+                    break;
             }
+                if (s.equals("L")){
+                    s = in.readUTF();
+                    leerTablero(s);
+                    interpretar();
+                    JOptionPane.showMessageDialog(null,
+                            "Tienes que mejorar.",
+                            "¡Has perdido!",
+                            JOptionPane.PLAIN_MESSAGE);
+                    break;
+                }
+                if (s.equals("D")){
+                    s = in.readUTF();
+                    System.out.println("me llega una " + s);
+                    leerTablero(s);
+                    interpretar();
+                    System.out.println("lanzo el mensaje");
+                    JOptionPane.showMessageDialog(null,
+                            "Los dos habéis jugado igual de mal",
+                            "¡Empate!",
+                            JOptionPane.PLAIN_MESSAGE);
+                    break;
+                }
+                leerTablero(s);
+                interpretar();
+                System.out.println("lo último que me llega es " +s );
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el tablero");
+            e.printStackTrace();
         }
+    }
+
+    private void interpretar() {
+        if (t[0][0] == 1) {
+            botones.get(0).setText("X");
+        } else if (t[0][0] == 2) {
+            botones.get(0).setText("O");
+        }
+        if (t[0][1] == 1) {
+            botones.get(1).setText("X");
+        } else if (t[0][1] == 2) {
+            botones.get(1).setText("O");
+        }
+        if (t[0][2] == 1) {
+            botones.get(2).setText("X");
+        } else if (t[0][2] == 2) {
+            botones.get(2).setText("O");
+        }
+        if (t[1][0] == 1) {
+            botones.get(3).setText("X");
+        } else if (t[1][0] == 2) {
+            botones.get(3).setText("O");
+        }
+        if (t[1][1] == 1) {
+            botones.get(4).setText("X");
+        } else if (t[1][1] == 2) {
+            botones.get(4).setText("O");
+        }
+        if (t[1][2] == 1) {
+            botones.get(5).setText("X");
+        } else if (t[1][2] == 2) {
+            botones.get(5).setText("O");
+        }
+        if (t[2][0] == 1) {
+            botones.get(6).setText("X");
+        } else if (t[2][0] == 2) {
+            botones.get(6).setText("O");
+        }
+        if (t[2][1] == 1) {
+            botones.get(7).setText("X");
+        } else if (t[2][1] == 2) {
+            botones.get(7).setText("O");
+        }
+        if (t[2][2] == 1) {
+            botones.get(8).setText("X");
+        } else if (t[2][2] == 2) {
+            botones.get(8).setText("O");
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        JButton btn = (JButton) e.getSource();
+        if (turno) {
+            if (jugar(btn)) {
+                turno = false;
+            } else {
+                JOptionPane.showMessageDialog(null,
+                        "No intentes hacer trampas",
+                        "¿Pero no ves que está ocupada?",
+                        JOptionPane.PLAIN_MESSAGE);
+            }
+        } else {
+            if (contadorTrampas < 2) {
+                if (this.id == '1') {
+                    JOptionPane.showMessageDialog(null,
+                            "Es el turno del jugador 2",
+                            "Espera tu turno",
+                            JOptionPane.PLAIN_MESSAGE);
+                } else if (this.id == '2') {
+                    JOptionPane.showMessageDialog(null,
+                            "Es el turno del jugador 1",
+                            "Espera tu turno",
+                            JOptionPane.PLAIN_MESSAGE);
+                }
+            } else {
+                JOptionPane.showMessageDialog(null,
+                        "Deja al otro jugador pensar dónde quiere colocar su ficha.",
+                        "QUE TE ESPERES, IMPACIENTE",
+                        JOptionPane.PLAIN_MESSAGE);
+            }
+            contadorTrampas++;
+        }
+    }
+
+    private boolean jugar(JButton btn) {
+        int index = botones.indexOf(btn);
+        if (!sustituir(index)) {
+            return false;
+        }
+        if (this.id == '1') {
+            btn.setText("X");
+        } else if (this.id == '2') {
+            btn.setText("O");
+        } else {
+            System.err.println("ID no especificado");
+        }
+
+        try {
+            out.writeUTF(s);
+        } catch (IOException ex) {
+            System.out.println("Error. No he podido enviar el tablero");
+            ex.printStackTrace();
+        }
+        return true;
     }
 
     private void leerTablero(String s) {
@@ -46,83 +197,14 @@ public class Cliente extends Vista implements ActionListener, Runnable {
         t[2][0] = Integer.parseInt(String.valueOf(s.charAt(6)));
         t[2][1] = Integer.parseInt(String.valueOf(s.charAt(7)));
         t[2][2] = Integer.parseInt(String.valueOf(s.charAt(8)));
-        System.out.println("Mmm... a ver si te toca:");
+
         if (s.charAt(s.length() - 1) == id) {
-            System.out.println("Te toca");
             this.turno = true;
         }
     }
 
-    private void mostrarTablero() {
-        for (int i = 0; i < TOTAL_FILAS; i++) {
-            for (int j = 0; j < TOTAL_COLUMNAS; j++) {
-                System.out.print(this.t[i][j]);
-                if (j == TOTAL_COLUMNAS - 1) {
-                    System.out.println();
-                }
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        new Cliente();
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-        JButton btn = (JButton) e.getSource();
-        if (turno)
-            jugar(btn);
-        turno = false;
-    }
-
-    private boolean jugar(JButton btn) {
-        if (this.id == 'U') {
-            btn.setText("X");
-        } else if (this.id == 'D') {
-            btn.setText("O");
-        } else {
-            System.err.println("ID no especificado");
-        }
-
-        btn.setText("X");
-        int index = botones.indexOf(btn);
-        if (!sustituir(index)) {
-            return false;
-        }
-        try {
-            out.writeUTF(s);
-        } catch (IOException ex) {
-            System.out.println("Error. No he podido enviar el tablero");
-            ex.printStackTrace();
-        }
-        return true;
-    }
-
-    private boolean sustituir(int index) {
-        if (Integer.parseInt(String.valueOf(s.charAt(index))) != 0) {
-            System.err.println("La casilla está ocupada");
-            return false;
-        }
-        if (s.substring(s.length() - 1).equals("U")) {
-            s = s.substring(0, index) + "1" + s.substring(index + 1);
-            s = s.substring(0, s.length() - 1);
-            s += "D";
-            return actualizarT(index);
-        } else if (s.substring(s.length() - 1).equals("D")) {
-            s = s.substring(0, index) + "2" + s.substring(index + 1);
-            s = s.substring(0, s.length() - 1);
-            s += "U";
-            return actualizarT(index);
-        } else {
-            System.err.println("Error en el tablero. El turno no está definido");
-            return false;
-        }
-    }
 
     private boolean actualizarT(int index) {
-        mostrarTablero();
         switch (index) {
             case 0:
                 if (t[0][0] != 0) {
@@ -215,42 +297,68 @@ public class Cliente extends Vista implements ActionListener, Runnable {
                 }
                 break;
         }
-        System.out.println("\nel tablero posterior es ");
-        mostrarTablero();
         return true;
     }
 
-    @Override
-    public void run() {
-        System.out.println("buenas");
-        iniciar();
-        System.out.println("he iniciado");
+    private void iniciar() {
+        contadorTrampas = 0;
+        try {
+            System.out.println("Esperando jugadores...");
+            //leemos la orden de jugar
+            s = in.readUTF();
+            //recibimos el id
+            s = in.readUTF();
+            //lo convertimos a char
+            this.id = s.charAt(0);
+        } catch (IOException e) {
+            System.out.println("Error al leer el ID");
+            e.printStackTrace();
+        }
+        if (id != '1' && id != '2') {
+            System.err.println("Ya hay dos jugadores conectados.");
+            System.exit(0);
+        }
+        System.out.println("¡A jugar!");
+        t = new int[TOTAL_FILAS][TOTAL_COLUMNAS];
+        for (int i = 0; i < TOTAL_FILAS; i++) {
+            for (int j = 0; j < TOTAL_COLUMNAS; j++) {
+                this.t[i][j] = 0;
+            }
+        }
         for (JButton boton : botones) {
             boton.addActionListener(this);
         }
-        try {
-            conn = new Socket(HOST, PUERTO);
-            in = new DataInputStream(conn.getInputStream());
-            out = new DataOutputStream(conn.getOutputStream());
-            System.out.println("voy a leer");
-            int i = in.read();
-            System.out.println(" i es " + i);
-            if (i == 1) {
-                this.id = 'U';
-            } else if (i == 2) {
-                this.id = 'D';
-            } else {
-                System.err.println("Error. Ya hay dos jugadores.");
-                System.exit(0);
-            }
-            while (true) {
-                s = in.readUTF();
-                leerTablero(s);
+    }
 
-            }
-            //mostrarTablero();
-        } catch (IOException e) {
-            e.printStackTrace();
+    private boolean sustituir(int index) {
+        if (Integer.parseInt(String.valueOf(s.charAt(index))) != 0) {
+            System.err.println("La casilla está ocupada");
+            return false;
         }
+        if (s.substring(s.length() - 1).equals("1")) {
+            s = s.substring(0, index) + "1" + s.substring(index + 1);
+            return actualizarT(index);
+        } else if (s.substring(s.length() - 1).equals("2")) {
+            s = s.substring(0, index) + "2" + s.substring(index + 1);
+            return actualizarT(index);
+        } else {
+            System.err.println("Error en el tablero. El turno no está definido");
+            return false;
+        }
+    }
+
+    private void mostrarTablero() {
+        for (int i = 0; i < TOTAL_FILAS; i++) {
+            for (int j = 0; j < TOTAL_COLUMNAS; j++) {
+                System.out.print(t[i][j]);
+                if (j == TOTAL_COLUMNAS - 1) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Cliente();
     }
 }
--- Practica 5/src/ejercicio4/Servidor.java
+++ Practica 5/src/ejercicio4/Servidor.java
@@ -3,46 +3,201 @@ package ejercicio4;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
 
 /**
- *
- * enviar a TODOS los clientes su ID
- *
- *
+ * TODO: gestionar empate, el ultimo jugador se queda esperando a que tiren
  */
 
 
 public class Servidor {
     private ServerSocket ss;
-    private DataOutputStream out;
-    private DataInputStream in;
+    private DataOutputStream out1; //flujo de salida
+    private DataOutputStream out2;
+    private DataInputStream in1; //flujo de entrada
+    private DataInputStream in2;
+    private String t; //tablero
+    private ArrayList<Socket> clientes;
 
     public Servidor() {
         try {
             //se inicializa el serversocket
             ss = new ServerSocket(5555);
-            ArrayList<Socket> clientes;
-            int i = 1;
-            while (true) {
-                clientes = new ArrayList<>();
-                //se crea un socket que espera a que llegue un cliente
-                Socket cliente = ss.accept();
-                //se lanza un hilo de la clase ClientHandler que gestiona la conexión de manera independiente a esta clase
-                new ClientHandler(cliente,clientes,i).start();
-                i++;
-                if (i == 3){
-                    break;
-                }
-            }
+            clientes = new ArrayList<>();
+
+            Socket cliente1 = ss.accept();
+            in1 = new DataInputStream(cliente1.getInputStream());
+            out1 = new DataOutputStream(cliente1.getOutputStream());
+            clientes.add(cliente1);
+
+            Socket cliente2 = ss.accept();
+            in2 = new DataInputStream(cliente2.getInputStream());
+            out2 = new DataOutputStream(cliente2.getOutputStream());
+            clientes.add(cliente2);
+
+            enviarATodos("jugar");
+            gestionarConexion();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    private void gestionarConexion() {
+        try {
+            //enviamos el id a cada cliente
+            out1.writeUTF("1");
+            out2.writeUTF("2");
+            t = "0000000001";
+            //enviamos el tablero
+            enviarATodos(t);
+            int turno = 1;
+            while (true) {
+                //leemos el tablero
+                if (turno == 1) {
+                    t = in1.readUTF();
+                    turno++;
+                } else if (turno == 2) {
+                    t = in2.readUTF();
+                    turno--;
+                }
+                System.out.println("tablero recibido: " + t);
+                int r = comprobarFinal();
+                if (r != 0) {
+                    System.out.println("r = " + r);
+                    resultado(r);
+                    break;
+                }
+                enviarATodos(cambiarOrden(t));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private String cambiarOrden(String t) {
+        String nuevoT = t.substring(0, t.length() - 1);
+        System.out.println("el orden es " + t.charAt(t.length() - 1));
+        if (t.charAt(t.length() - 1) == '1') {
+            nuevoT += "2";
+            return nuevoT;
+        } else if (t.charAt(t.length() - 1) == '2') {
+            nuevoT += "1";
+            return nuevoT;
+        } else {
+            System.err.println("El string que he recibido no contiene el turno");
+            return null;
+        }
+    }
+
+    //clase que envía el paquete a todos los clientes
+    private void enviarATodos(String s) throws IOException {
+        for (Socket soc : clientes) {
+            //se crea un flujo de salida
+            DataOutputStream out = new DataOutputStream(soc.getOutputStream());
+            //se envía el paquete a cada cliente a través del flujo
+            out.writeUTF(s);
+        }
+    }
+
+    private int comprobarFinal() {
+        //comprobar filas
+        for (int i = 0; i < t.length(); i++) {
+        }
+        if (t.charAt(0) == t.charAt(1) && t.charAt(0) == t.charAt(2)) {
+            if (t.charAt(0) != '0') {
+                return Character.getNumericValue(t.charAt(0));
+            }
+        } else if (t.charAt(3) == t.charAt(4) && t.charAt(3) == t.charAt(5)) {
+            if (t.charAt(3) != '0') {
+                return Character.getNumericValue(t.charAt(3));
+            }
+        } else if (t.charAt(6) == t.charAt(7) && t.charAt(6) == t.charAt(8)) {
+            if (t.charAt(6) != '0') {
+                return Character.getNumericValue(t.charAt(6));
+            }
+        }
+        //comprobar columnas
+        if (t.charAt(0) == t.charAt(3) && t.charAt(0) == t.charAt(6)) {
+            if (t.charAt(0) != '0') {
+                return Character.getNumericValue(t.charAt(0));
+            }
+        } else if (t.charAt(1) == t.charAt(4) && t.charAt(1) == t.charAt(7)) {
+            if (t.charAt(1) != '0') {
+                return Character.getNumericValue(t.charAt(1));
+            }
+        } else if (t.charAt(2) == t.charAt(5) && t.charAt(2) == t.charAt(8)) {
+            if (t.charAt(2) != '0') {
+                return Character.getNumericValue(t.charAt(2));
+            }
+
+        }
+
+        //comprobar diagonales
+        if (t.charAt(0) == t.charAt(4) && t.charAt(0) == t.charAt(8)) {
+            if (t.charAt(0) != '0') {
+                return Character.getNumericValue(t.charAt(0));
+            }
+        } else if (t.charAt(2) == t.charAt(4) && t.charAt(2) == t.charAt(6)) {
+            if (t.charAt(2) != '0') {
+                return Character.getNumericValue(t.charAt(2));
+            }
+        }
+        if (lleno()) {
+            return 3;
+        }
+        return 0;
+    }
+
+    private void resultado(int r) {
+        if (r == 1) {
+            //JOptionPane.showMessageDialog(null,"","",JOptionPane.PLAIN_MESSAGE);
+            System.out.println("Gana el jugador 1");
+            try {
+                out1.writeUTF("W");
+                out2.writeUTF("L");
+                out2.writeUTF(t);
+            } catch (IOException e) {
+                System.out.println("Error. No se ha podido notificar de la victoria 1");
+                e.printStackTrace();
+            }
+        } else if (r == 2) {
+            System.out.println("Gana el jugador 2");
+            try {
+                out1.writeUTF("L");
+                out1.writeUTF(t);
+                out2.writeUTF("W");
+            } catch (IOException e) {
+                System.out.println("Error. No se ha podido notificar de la victoria 2");
+                e.printStackTrace();
+            }
+        } else if (r == 3) {
+            System.out.println("Empate");
+            try {
+                out1.writeUTF("D");
+                out2.writeUTF("D");
+                try {
+                    Thread.currentThread().sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } catch (IOException e) {
+                System.out.println("Error. No se ha podido notificar del empate");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean lleno() {
+        if (t.indexOf('0') == -1){
+            System.out.println("está lleno");
+            return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
